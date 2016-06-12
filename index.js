9f41da7b5a8c88fe0a291489d35ea4e465208e25
Anton Medvedev <anton@medv.io>
2016-06-12 16:04:40 +0700

    Add hot module replacement.

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ import 'todomvc-app-css/index.css'
 
 const store = configureStore();
 const context = bindActionCreators(TodoActions, store.dispatch);
-const view = Monkberry.render(App, document.body, {context});
+
+let view = Monkberry.render(App, document.body, {context});
 
 const listener = () => view.update(store.getState());
 store.subscribe(listener);
 listener();
+
+if (module.hot) {
+  module.hot.accept('./components/App.monk', () => {
+    view.remove();
+    const App = require('./components/App.monk');
+    view = Monkberry.render(App, document.body, {context});
+    view.update(store.getState());
+  });
+}
